6457ccb32e666cfe2430316c58f167b0fa83cb34
Andrey Karamnoff <karamnoff@gmail.com>
2020-11-17 18:56:33 +0300

    add redux actions for channel addition

--- src/actions/channelsActions.js
+++ src/actions/channelsActions.js
@@ -1,6 +1,37 @@
+import axios from 'axios';
 import {
+  ADD_CHANNEL_REQUEST,
+  ADD_CHANNEL_SUCCESS,
+  ADD_CHANNEL_FAILURE,
   SET_CURRENT_CHANNEL_ID,
 } from './types';
+import routes from '../routes';
+
+const addChannelRequest = () => ({ type: ADD_CHANNEL_REQUEST });
+
+export const addChannelSucess = (payload) => ({
+  type: ADD_CHANNEL_SUCCESS,
+  payload,
+});
+
+export const addChannelFailure = (payload) => ({
+  type: ADD_CHANNEL_FAILURE,
+  payload,
+});
+
+export const addChannel = (name) => async (dispatch) => {
+  dispatch(addChannelRequest());
+
+  try {
+    const data = {
+      data: { attributes: { name } },
+    };
+
+    await axios.post(`${routes.channelsPath()}`, data);
+  } catch (error) {
+    dispatch(addChannelFailure(error));
+  }
+};
 
 export const setCurrentChannelId = (payload) => ({
   type: SET_CURRENT_CHANNEL_ID,
--- src/reducers/channelsReducer.js
+++ src/reducers/channelsReducer.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-param-reassign */
 
 import gon from 'gon';
-import { SET_CURRENT_CHANNEL_ID } from '../actions/types';
+import {
+  ADD_CHANNEL_REQUEST,
+  ADD_CHANNEL_SUCCESS,
+  ADD_CHANNEL_FAILURE,
+  SET_CURRENT_CHANNEL_ID,
+} from '../actions/types';
 
 const initialState = {
   channels: gon.channels || [],
   currentChannelId: gon.currentChannelId || null,
+  isLoading: false,
+  error: null,
   modal: {
     isOpened: false,
     type: null,
@@ -15,6 +22,20 @@ const initialState = {
 
 const channelsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_CHANNEL_REQUEST:
+      return { ...state, isLoading: true };
+
+    case ADD_CHANNEL_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        channels: [...state.channels, action.payload],
+      };
+
+    case ADD_CHANNEL_FAILURE:
+      return { ...state, isLoading: false, error: action.payload };
+
     case SET_CURRENT_CHANNEL_ID:
       return { ...state, currentChannelId: action.payload };
     default:
