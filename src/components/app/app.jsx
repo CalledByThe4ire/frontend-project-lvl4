fd0e3ccb94f90130957226baa6827a46c21c2924
Andrey Karamnoff <karamnoff@gmail.com>
2020-11-21 09:34:40 +0300

    simplify project structure

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,84 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import NicknameProvider from '../../context/nickname';
-import Chat from '../chat';
-import store from '../../store';
+import React, { useContext, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import io from 'socket.io-client';
+import gon from 'gon';
+import { NicknameContext } from '../../context/nickname';
+import {
+  addChannelSuccess,
+  renameChannelSuccess,
+  removeChannelSuccess,
+  setCurrentChannelId,
+} from '../../actions/channelsActions';
+import {
+  addMessageSuccess,
+  removeChannelMessages,
+} from '../../actions/messagesActions';
+import NavBar from '../navbar';
+import Layout from '../layout';
+import Channels from '../channels';
+import Messages from '../messages';
 
-const App = () => (
-  <Provider store={store}>
-    <NicknameProvider>
-      <Chat />
-    </NicknameProvider>
-  </Provider>
-);
+const App = () => {
+  const socket = io();
 
+  const dispatch = useDispatch();
+
+  const nickname = useContext(NicknameContext);
+
+  useEffect(() => {
+    socket.on('newMessage', (msg) => {
+      const {
+        data: {
+          attributes: { message: body, id, channelId },
+        },
+      } = msg;
+
+      dispatch(
+        addMessageSuccess({
+          body,
+          id,
+          nickname,
+          channelId,
+        }),
+      );
+    });
+
+    socket.on('newChannel', (channel) => {
+      const {
+        data: {
+          attributes: { name, removable, id },
+        },
+      } = channel;
+
+      dispatch(addChannelSuccess({ name, removable, id }));
+    });
+
+    socket.on('renameChannel', (channel) => {
+      const {
+        data: {
+          attributes: { name, id },
+        },
+      } = channel;
+
+      dispatch(renameChannelSuccess({ name, id }));
+    });
+
+    socket.on('removeChannel', (channelId) => {
+      const {
+        data: { id },
+      } = channelId;
+
+      dispatch(removeChannelSuccess({ id }));
+      dispatch(removeChannelMessages({ id }));
+      dispatch(setCurrentChannelId(gon.currentChannelId));
+    });
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Layout left={<Channels />} right={<Messages />} />
+    </>
+  );
+};
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import NicknameProvider from './context/nickname';
+import store from './store';
 import '../assets/application.scss';
 import App from './components/app';
 
@@ -11,4 +14,11 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <NicknameProvider>
+      <App />
+    </NicknameProvider>
+  </Provider>,
+  document.getElementById('app'),
+);
